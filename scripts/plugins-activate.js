50ff3947685ecf1cff303ddb45927bf083be95c3
Megha BM <megha.bm@gop2.com>
2023-08-03 16:49:37 +0530

    updated bootstrap and jquery owlcarousel  fancybox

--- scripts/plugins-activate.js
+++ scripts/plugins-activate.js
@@ -1,53 +1,57 @@
-(function($jq) {
-  $jq(document).ready(function() {
+(function($) {
+  $(document).ready(function() {
     // Closes the sidebar menu
-    $jq(".menu-toggle").click(function(e) {
+    $(document).on("click", ".menu-toggle", function(e) {
       e.preventDefault();
-      $jq("#sidebar-wrapper").toggleClass("active");
-      $jq(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-      $jq(this).toggleClass("active");
-    });
+      $("#sidebar-wrapper").toggleClass("active");
+      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+      $(this).toggleClass("active");
+  });
+
     // Smoothscroll script
-    $jq(".smooth-scroll").click(function(e) {
+    $(document).on("click", ".smooth-scroll", function(e) {
       e.preventDefault();
-      var dis = $jq(this),
-        target = dis.attr("href"),
-        offset = parseInt($jq(target).offset().top),
-        header = $jq(".sidebar-nav");
+      var dis = $(this),
+          target = dis.attr("href"),
+          offset = parseInt($(target).offset().top),
+          header = $(".sidebar-nav");
+
       dis.addClass("active").parent().siblings().find(".smooth-scroll").removeClass("active");
-      $jq('html,body').stop().animate({ scrollTop: offset }, 200);
-      $jq(".menu-toggle").trigger("click");
-    });
+      $('html,body').stop().animate({ scrollTop: offset }, 200);
+      $(".menu-toggle").trigger("click");
+  });
+
     // Closes responsive menu when a scroll trigger link is clicked
-    $jq('#sidebar-wrapper .js-scroll-trigger').click(function() {
-      $jq("#sidebar-wrapper").removeClass("active");
-      $jq(".menu-toggle").removeClass("active");
-      $jq(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    });
-    //  TESTIMONIALS CAROUSEL HOOK
-    var owl = $jq('#customers-testimonials');
+    $(document).on("click", "#sidebar-wrapper .js-scroll-trigger", function() {
+      $("#sidebar-wrapper").removeClass("active");
+      $(".menu-toggle").removeClass("active");
+      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+    // TESTIMONIALS CAROUSEL HOOK
+    var owl = $('#customers-testimonials');
     owl.owlCarousel({
-      loop: true,
-      center: true,
-      items: 3,
-      margin: 0,
-      autoplay: true,
-      dots: true,
-      autoplayTimeout: 8500,
-      smartSpeed: 450,
-      responsive: {
-        0: {
-          items: 1
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
         },
-        768: {
-          items: 2
-        },
-        1170: {
-          items: 3
-        }
-      },
-      navigation: true,
-      navigationText: true
+        navigation: true,
+        navigationText: true
     });
   });
-})(jQuery)
+})(jQuery);
